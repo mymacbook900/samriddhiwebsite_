e80f971a1f996602faab7cfd1ef4348cee47eb85
Umashankar Patel <umashankarpatel78980@gmail.com>
2026-02-23 17:00:10 +0530

    23 feb changes uma

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://samridhiwebbackend.onrender.com/api';
+const API_BASE_URL = 'https://samridhiwebbackend-1.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
